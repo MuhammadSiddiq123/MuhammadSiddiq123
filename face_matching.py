afcba926ad4b44dedf3363dc7d7d5cea08947a9b
ProProgrammer <99825789+MuhammadSiddiq123@users.noreply.github.com>
2022-03-17 21:29:22 +0500

    Update face_matching.py

--- face_matching.py
+++ face_matching.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 17 08:28:29 2022
-
+images verification/face matching through python and face_recognition model
 @author: SiddiQ
 """
 
-import face_recognition
-known_image = face_recognition.load_image_file(r"D:\images_set\akrammm.jpeg")
-unknown_image = face_recognition.load_image_file(r"D:\images_set\akram.jpeg")
+import face_recognition     # importing the face recognition model
 
-biden_encoding = face_recognition.face_encodings(known_image)[0]
-unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+# In the below two line we just passing the paths of the images that we are going to verify
+Img1_Path = face_recognition.load_image_file(r"D:\images_set\akrammm.jpeg")
+Img2_Path = face_recognition.load_image_file(r"D:\images_set\akram.jpeg")
 
-results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+# The below two lines of code will just encode the images from there paths
+Img1_encoding = face_recognition.face_encodings(Img1_Path)[0]
+Img2_encoding = face_recognition.face_encodings(Img1_Path)[0]
+
+# the below line will just compare the two encoded images weather it matchs or not
+results = face_recognition.compare_faces([Img1_encoding], Img2_encoding)
 print(results)
